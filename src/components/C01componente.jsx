dc77d1fd3c72d5581cd36ea66c75963aa3b1ebe6
Benjamin A <benjamin.alvarado1510@gmail.com>
2023-10-13 09:14:45 -0500

    version 2

---
+++ src/components/C01componente.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const C01componente = () => {
+  return (
+    <div>
+      <h1>componente01 </h1>
+      PAGINA DE PRUEBA
+    </div>
+  )
+}
+
+export default C01componente
+
+
+
---
+++ src/components/P4variable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const variable = (msj) => {
+  return (
+    <div>
+      <h1>hola mundo... lectura a una variable </h1>
+      <h2>Mensaje{msj.xvarible}</h2>
+    </div>
+  )
+}
+
+export default variable;
