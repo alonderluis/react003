1916215903d7df0c4e5ea6aefdc3cf240034d9fd
Benjamin A <benjamin.alvarado1510@gmail.com>
2023-10-27 09:21:58 -0500

    version_6 se muestra array con datos de bd

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-//import C01componente from './components/C01componente';
-//import P4variable from './components/P4variable';
-import AppForm from './components/AppForm';
+//import logo from './logo.svg';
+//import './App.css';
 import { useState } from 'react';
+import AppForm from './components/AppForm';
+import { collection, doc, onSnapshot, query } from 'firebase/firestore';
+import { db } from './firebase/firebase';
 
 function App() {
-  ///// READ - LECTURA - fnRead //////
-  const [docBD, setdocBD] = useState([]);
+  ///////// READ - Lectura - fnRead ////////////
+  const [docBD, setDocBD] = useState([]);
   const fnRead = () => {
+    const xColecionConQuery = query(collection(db, "persona"));
+    const unsubscribe = onSnapshot(xColecionConQuery, (xDatosBD) => {
+      const xDoc = [];
+      xDatosBD.forEach((docV)=>{
+        xDoc.push({id:docV.id, ...docV.data()});
+        //console({id:doc.id, ...doc.data()});
+      });
+      setDocBD(xDoc);
+    });
   }
-  ///// DELETE - ELIMINAR - fnDelete /////
+  fnRead();
+  console.log(docBD);
+  ///////// DELETE - Eliminar - fnDelete ///////
   const [idActual, setIdActual] = useState("");
-  const fnDelete =(xId) =>{
-
+  const fnDelete = (xId) => {
   }
+  
   return (
-    <div style={{background:"yellow", width:"350px", padding:"10px", textAlign:"center"}}>
-      <h1>App.js</h1>
-      <AppForm {...{idActual}}></AppForm>
+    <div style={{background:"yellow", width:"350px", 
+     padding:"10px"}}>
+      <AppForm {...{idActual}} />
       <i class="large material-icons">insert_chart</i>
 
-      <p>1. Frodoncio Famio   23 Masculino    ---- x - A</p>
-      <p>2. Rosa Merino    25 Femenino     ---- x - A</p>
-      <p>3. Max Orlando 22 Masculino    ---- x - A</p>
+      <p>1. Juan Manuel 23 Masculino ---- x -  A</p>
+      <p>2. Rosa Maria  25 Femenino  ---- x -  A</p>
+      <p>3. Luis Miguel 40 Masculino ---- x -  A</p>
     </div>
   );
 }
 
 export default App;
+
