5ff818aa7b4ec47ea46b2baedbfa40da5acdf27d
Benjamin A <benjamin.alvarado1510@gmail.com>
2023-10-27 08:22:51 -0500

    version_5. ya registra previa validacion y usa el idActual para Guardar o Actualizar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 //import C01componente from './components/C01componente';
+//import P4variable from './components/P4variable';
 import AppForm from './components/AppForm';
+import { useState } from 'react';
 
- 
 function App() {
+  ///// READ - LECTURA - fnRead //////
+  const [docBD, setdocBD] = useState([]);
+  const fnRead = () => {
+  }
+  ///// DELETE - ELIMINAR - fnDelete /////
+  const [idActual, setIdActual] = useState("");
+  const fnDelete =(xId) =>{
+
+  }
   return (
-  <div style={{background:"yellow", width:"350px" , padding:"10px",}}>
-    <h1>App.js</h1>
-    <AppForm></AppForm>
-    <i class="large material-icons">insert_chart</i>
-    <p>1. Juan Manuel 23 Masculino ----X----A </p>
-    <p>1. Rosa Maria  25 Femenino  ----X----A </p>
-    <p>1. Luis Miguel 22 Masculino ----X----A </p>
+    <div style={{background:"yellow", width:"350px", padding:"10px", textAlign:"center"}}>
+      <h1>App.js</h1>
+      <AppForm {...{idActual}}></AppForm>
+      <i class="large material-icons">insert_chart</i>
+
+      <p>1. Frodoncio Famio   23 Masculino    ---- x - A</p>
+      <p>2. Rosa Merino    25 Femenino     ---- x - A</p>
+      <p>3. Max Orlando 22 Masculino    ---- x - A</p>
     </div>
   );
 }
--- src/components/AppForm.js
+++ src/components/AppForm.js
@@ -1,16 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { db } from "../firebase/firebase";
+import { addDoc, collection } from 'firebase/firestore';
 
-const AppForm = (msj) => {
-  return (
-    <div style={{background:"orange",padding:"10px",textAlign:"center",}}>
-         <h>AppForm.js</h> <br/>
-        <input placeholder='Nombres...'/><br/>
-        <input placeholder='Edad...'/><br/>
-        <input placeholder='Genero...'/><br/>
-        <button>GUARDAR</button>
+const Appform = (props) => {
+  const camposRegistro = {nombre:"", edad:"", genero:""}
+  const [objeto, setObjeto] = useState(camposRegistro);
+////// GUARDAR / ACTUALIZAR /////
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+    try {
+      if(props.idActual==""){
+        if(validarForm ()){
+          addDoc(collection(db, 'persona'), objeto);
+          console.log("Se Guardo con exito..");
+        }else{
+          console.log("No se guardo");
+        }
         
-        </div>
+      }else{
+        console.log("Actualizar en BD");
+      }
+      setObjeto(camposRegistro);
+    } catch (error){
+      console.error();
+    }
+  }
+
+  const validarForm = () => {
+    if(objeto.nombre==="" || /^\s+$/.test(objeto.nombre)){
+      alert("Escriba nombres...");
+      return false;
+    }
+    if(objeto.nombre==="" || /^\s+$/.test(objeto.nombre)){
+        alert("Escriba edad...");
+        return false;
+    }
+    if(objeto.nombre==="" || /^\s+$/.test(objeto.nombre)){
+          alert("Escriba genero...");
+          return false;
+    }
+  
+    return true;
+  }
+  const manejarCambiosEntrada = (e) =>{
+    console.log(e.target.value);
+    const {name, value} = e.target;
+    console.log(name, value);
+
+    setObjeto({...objeto, [name]:value});
+    console.log("nnnnn");
+  }
+
+  return (
+    <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
+      <h>AppForm.js</h> <br/> 
+      <form onSubmit={manejarEnvio} >
+        <input onChange={manejarCambiosEntrada} value={objeto.nombre} name='nombre' type='text' placeholder='Nombres...'></input><br></br>
+        <input onChange={manejarCambiosEntrada} value={objeto.edad} name='edad' type='text' placeholder='Edad...'></input><br></br>
+        <input onChange={manejarCambiosEntrada} value={objeto.genero} name='genero' type='text' placeholder='Género...'></input><br></br>
+        <br></br>
+        <button>
+          { props.idActual===""? "Guardar":"Actualizar" }
+        </button>
+      </form>
+    </div>
   )
 }
 
-export default AppForm;
+export default Appform
