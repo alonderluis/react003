761b1e2b96e49a26dc1cf6e08132fd01cc22fc60
Benjamin A <benjamin.alvarado1510@gmail.com>
2023-11-03 09:18:05 -0500

    v8. autenticacion y ruteo en proyecto ok

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,10 @@
-// Import the functions you need from the SDKs you need
+/*// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 import {getAuth} from "firebase/auth";
 import {getStorage, ref, uploadBytes } from "firebase/storage";
-import {Firestore, addDoc, collection, getDoc, getDocs, Query, where, setDoc, deleteDoc, updateDoc, getFirestore } from "firebase/firestore";
+//import {Firestore, addDoc, collection, getDoc, getDocs, Query, where, setDoc, deleteDoc, updateDoc, getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -28,3 +28,4 @@ const analytics = getAnalytics(app);
 const auth = getAuth(app);
 export const db = getFirestore(app);
 const storage = getStorage(app);
+*/
